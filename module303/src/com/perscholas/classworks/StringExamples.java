216632622492824d638f56510cca17aed06b4cad
Arthur <odthientho@gmail.com>
2024-10-04 13:28:24 -0400

    finish the Reading String GLAB 303.3.2 & more stuffs from String lessons

--- module303/src/com/perscholas/classworks/StringExamples.java
+++ module303/src/com/perscholas/classworks/StringExamples.java
@@ -19,5 +19,45 @@ public class StringExamples {
         System.out.println(s.toUpperCase());
         System.out.println(s.concat("ghijk")); // concat is to + 2 strings together
 
+        String r = "abc123abc";
+        // have to assign the out result into the r
+        System.out.println(r.replace("a", "z"));
+        System.out.println(r.replace("abc", "xyz"));
+
+        System.out.println(r.replaceAll("\\d", "x"));
+
+        // --- Substrings ---
+        String filename = "someimage.jpg";
+        // position that first . appearance
+        System.out.println(filename.indexOf('.'));
+        // position that last . appearance
+        System.out.println(filename.lastIndexOf('.'));
+
+        // get the name of the file
+        System.out.println(filename.substring(0, filename.lastIndexOf('.')));
+        // Get the format of the file
+        System.out.println(filename.substring(filename.lastIndexOf('.') + 1));
+
+        String s1 = new String("abc");
+        String s2 = new String("aBc");
+
+        // *********  very IMPORTANT ************* //
+
+        // this is never correct for strings
+        System.out.println(s1 == s2);
+
+        // ALWAYS correct way to compare 2 strings
+        System.out.println(s1.equals(s2));
+        // compares 2 string without case of sensitivity
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+        String n = "101";
+        int n1 = Integer.parseInt(n);
+        System.out.println(n1);
+
+        // String in Java are immutable
+        // They cannot be changed when they are created.
+        // They introduce StringBuffer as a peer class of String
+
     }
 }
