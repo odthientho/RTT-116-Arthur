6337bf948721fe82fd520c114985cc6071f258bd
Arthur <odthientho@gmail.com>
2024-10-16 17:54:58 -0400

    finish GLAB 303.11.3 ArrayList of User

---
+++ module303/src/com/perscholas/homeworks/GLAB303_11_3/showData.java
@@ -0,0 +1,17 @@
+package com.perscholas.homeworks.GLAB303_11_3;
+
+import java.util.ArrayList;
+
+public class showData {
+    public static void main(String[] args) {
+        // instantioation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue: mybooklist)
+        {
+            // ---- invoking getter method for geting Data---------
+            System.out.println(showValue.getNumber() + " " + showValue.getName() +" "+ showValue.getCategory() +" "+ showValue.getAuthor());
+        }
+    }
+
+}
