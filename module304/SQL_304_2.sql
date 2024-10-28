1f8c98cfe3ef8fd4d166fd5131a28403b4f957ad
Arthur <odthientho@gmail.com>
2024-10-28 11:18:57 -0400

    insert and delete data from table

--- module304/SQL_304_2.sql
+++ module304/SQL_304_2.sql
@@ -1,1 +1,10 @@
+-- select all customers
 select * from customers;
+-- insert information into a table offices
+insert into offices (city, phone, address_line1, address_line2, state, country, postal_code, territory)
+values ('Alpharetta', '714589', '792 Hello', 'Lawrence', 'GA', 'Vietnam', '30077', 'Hello');
+-- select all from offices
+select * from offices;
+-- delete from offices where city is Alpharetta
+delete from offices where city='Alpharetta';
+
