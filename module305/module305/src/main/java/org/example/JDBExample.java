1f6f6099c02c22975272fa74147372088c75f28a
Arthur <odthientho@gmail.com>
2024-11-06 11:32:07 -0500

    database connection example

---
+++ module305/module305/src/main/java/org/example/JDBExample.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.sql.*;
+
+public class JDBExample {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String dburl = "jdbc:mysql://localhost:3306/classic_models";
+        String user = "root";
+        String password = "Abc@1234";
+        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(dburl, user, password);
+            String SelectSQL = "Select * FROM employees";
+            Statement stmt = connection.createStatement();
+            ResultSet result =  stmt.executeQuery(SelectSQL);
+            while(result.next())
+            {
+                String EmployeeID  = result.getString("id");
+                String fname = result.getString("firstname");
+                String lname  = result.getString("lastName");
+                System.out.println(EmployeeID +" | " + fname + " | "+ lname );
+            }
+            connection.close();
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
