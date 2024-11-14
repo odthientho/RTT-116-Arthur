5031ccd309a4ccc77d044311a4b8ca4ee0f6efde
Arthur <odthientho@gmail.com>
2024-11-14 13:00:01 -0500

    testing customerDAO

---
+++ module305/module305/src/test/java/org/example/database/dao/CustomerDAOTest.java
@@ -0,0 +1,28 @@
+package org.example.database.dao;
+
+import org.example.database.entity.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CustomerDAOTest {
+    private CustomerDAO customerDAO = new CustomerDAO();
+
+    @Test
+    public void testFindById() {
+        int givenCustomerId = 103;
+        String expectedCustomerName = "Atelier graphique";
+
+        Customer actualCustomer = customerDAO.findById(givenCustomerId);
+
+        Assertions.assertNotNull(actualCustomer);
+
+        Assertions.assertEquals(givenCustomerId, actualCustomer.getId());
+        Assertions.assertEquals(expectedCustomerName, actualCustomer.getCustomerName());
+    }
+
+    @Test
+    public void testFindByIdNull() {
+        Customer customer = customerDAO.findById(1);
+        Assertions.assertNull(customer);
+    }
+}
